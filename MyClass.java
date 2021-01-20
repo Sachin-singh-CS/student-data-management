47bf36f6c694c65e4bf5c18ea881f66e8a26b435
Sachin-singh-CS <77718982+Sachin-singh-CS@users.noreply.github.com>
2021-01-20 13:58:26 +0530

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,2 @@
+<iframe style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//ws-in.amazon-adsystem.com/widgets/q?ServiceVersion=20070822&OneJS=1&Operation=GetAdHtml&MarketPlace=IN&source=ac&ref=tf_til&ad_type=product_link&tracking_id=sachin031b-21&marketplace=amazon&region=IN&placement=B07V4L1KC5&asins=B07V4L1KC5&linkId=c9de579d94a9a252a78f82b7ad60d453&show_border=false&link_opens_in_new_window=false&price_color=333333&title_color=0066C0&bg_color=FFFFFF">
+    </iframe>
